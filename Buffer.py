c93b5d90269afe71e64ff0e557d278e00669985e
Gaurav21899 <gaurav21899@gmail.com>
2023-08-22 16:23:54 +0530

    Buffer

--- Buffer.py
+++ Buffer.py
@@ -1,4 +1,5 @@
 import arcpy
 
 # Setting the default workspace environment
-arcpy.env.workspace = D:\Sem-3\Programming for GIS-3\Practical_1_ProProject\Practical_1_ProProject\01_Running_Python_Scripts.gdb
+arcpy.env.workspace = r"D:\Sem-3\Programming for GIS-3\Practical_1_ProProject\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
+arcpy.analysis.Buffer("Wilsin_Schools.Buffer","500 meters")
