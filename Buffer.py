eba7250254eb48563d00334ae955d24987dbef1e
Gaurav21899 <gaurav21899@gmail.com>
2023-08-22 16:06:13 +0530

    Buffer.py

---
+++ Buffer.py
@@ -0,0 +1,4 @@
+import arcpy
+
+# Setting the default workspace environment
+arcpy.env.workspace = D:\Sem-3\Programming for GIS-3\Practical_1_ProProject\Practical_1_ProProject\01_Running_Python_Scripts.gdb
