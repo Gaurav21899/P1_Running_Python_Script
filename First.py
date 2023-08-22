3d0eec7fa79113f762c4a679a533e41c72e975d3
Gaurav21899 <gaurav21899@gmail.com>
2023-08-22 15:52:13 +0530

    First.py

--- First.py
+++ First.py
@@ -4,3 +4,23 @@ print(name)
 print(surname)
 
 print("My first name is {} and my surname is {}".format(name, surname))
+
+age = "22"
+print(type(age))
+print(type(surname))
+
+
+# converting string into integer
+
+age = int(age)
+print(type(age))
+
+# converting integer into string age = str(age)
+print(type(age))
+print(type(age))
+
+true_booleen = True
+false_booleen = False
+
+print(type(true-boolean))
+
