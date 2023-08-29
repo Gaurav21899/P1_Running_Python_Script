e4ffb8e590f4990a8a2893c69a00b1ecae083d4b
Gaurav21899 <gaurav21899@gmail.com>
2023-08-29 14:48:11 +0530

    Buffer of 600m

--- Buffer.py
+++ Buffer.py
@@ -1,8 +1,8 @@
-# import arcpy
-#
-# # Setting the default workspace environment
-# arcpy.env.workspace = r"D:\Sem-3\Programming for GIS-3\P1_Running_Python_Script\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
-# arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer_600","600 meters")
-#
-# print("Process Completed")
+import arcpy
+
+# Setting the default workspace environment
+arcpy.env.workspace = r"D:\Sem-3\Programming for GIS-3\P1_Running_Python_Script\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
+arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer_600","600 meters")
+
+print("Process Completed")
 
