2a9fb30e673f031f63395f4d620954bb46823c9d
Gaurav21899 <gaurav21899@gmail.com>
2023-08-29 16:43:07 +0530

    Buffer Distance

--- Buffer_2.py
+++ Buffer_2.py
@@ -4,10 +4,10 @@ import arcpy
 input_feature_class_path = r"D:\Sem-3\Programming for GIS-3\P1_Running_Python_Script\Practical_1_ProProject\01_Running_Python_Scripts.gdb\Wilson_Schools"
 
 # Path to the output buffer feature class
-output_buffer_path = r"D:\Sem-3\Programming for GIS-3\P1_Running_Python_Script\Practical_1_ProProject\Ouput.gdb\buffer_500m"
+output_buffer_path = r"D:\Sem-3\Programming for GIS-3\P1_Running_Python_Script\Practical_1_ProProject\Ouput.gdb\buffer_700m"
 
 # Buffer distance
-Distance = "500 meters"
+Distance = "700 meters"
 
 arcpy.analysis.Buffer(input_feature_class_path,output_buffer_path,Distance)
 
